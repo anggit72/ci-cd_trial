1ca93aa7355125fc7d2d56c4c7fd29f7d30d555d
anggit72 <anggit.indera@icloud.com>
2020-05-27 17:53:50 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var handleRequest = function(request, response) {
   response.writeHead(200);
-  response.end("Hello Demo Jenkins !!!");
+  response.end("Hello IFF !!!");
 }
 var www = http.createServer(handleRequest);
 www.listen(8080);
