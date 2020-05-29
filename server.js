b90d18c667d208823db3604c03056242d6dd71a0
anggit72 <anggit.indera@icloud.com>
2020-05-29 09:23:36 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var handleRequest = function(request, response) {
   response.writeHead(200);
-  response.end("Hello Infofabrica !!!");
+  response.end("Hello !!!");
 }
 var www = http.createServer(handleRequest);
 www.listen(8080);
